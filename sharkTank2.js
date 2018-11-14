04efecce04abd1001ce921f3bb534c84fd4c4085
Andoni <44707107+andonin213@users.noreply.github.com>
2018-11-14 11:23:55 -0500

    make the slide show actually work

--- sharkTank2.js
+++ sharkTank2.js
@@ -1,40 +1,55 @@
 // JavaScript Document
 $(document).ready(function(){
-	$("#slideImg2").hide();
-	$("#slideImg3").hide();
 	
 	var slideIndex = 1;
-	showSlide(slideIndex);
+	showDivs(slideIndex);
 	
 	function plusDivs(n){
-		showSlide(slideIndex += n);
+		showDivs(slideIndex += n);
 	}
 	
-function showSlide(n) {
-	var x = document.getElementsByClassName("mySlides");
-	if(n > x.length) {slideIndex = 1}
-	if( n<1) {slideIndex = x.length}
-	for (var i = 0; i < x.length; i++){
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlide");
+	if(n > x.length) {
+		slideIndex = 1;
+	}
+	if( n < 1) {
+		slideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++){
 		x[i].style.display = "none";
 	}
 	x[slideIndex-1].style.display = "block";
 }	
+	
+	$(".mySlide").cycle(
+		fx
+	);
 	
 });
 
 	var slideIndex = 1;
-	showSlide(slideIndex);
+	showDivs(slideIndex);
 	
 	function plusDivs(n){
-		showSlide(slideIndex += n);
+		showDivs(slideIndex += n);
 	}
 	
-function showSlide(n) {
-	var x = document.getElementsByClassName("mySlides");
-	if(n > x.length) {slideIndex = 1}
-	if( n<1) {slideIndex = x.length}
-	for (var i = 0; i < x.length; i++){
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlide");
+	if(n > x.length) {
+		slideIndex = 1;
+	}
+	if( n < 1) {
+		slideIndex = x.length;
+	}
+	for (i = 0; i < x.length; i++){
 		x[i].style.display = "none";
 	}
 	x[slideIndex-1].style.display = "block";
 }	
+
+
+$(".mySlide").cycle();
